ba00d2263f6d967b966137dce32eb6f11040993f
Sami Alsheikh <alsheikh@mit.edu>
2017-06-29 15:16:50 -0400

    fixed saliency

--- djangosite/design/urls.py
+++ djangosite/design/urls.py
@@ -5,5 +5,7 @@ from design import views
 urlpatterns = patterns('',
     url(r'^$',views.main),
     url(r'^create',views.createDesign),
+    url(r'^saveImage',views.saveImage),
+    url(r'^saveDesign',views.saveDesign),
     url(r'^requestSaliencyImage', views.createSaliencyImage)
 )
---
+++ optLayout/src/settings.cuh
@@ -0,0 +1,1 @@
+char *home_dir="/Users/samialsheikh/Desktop/interactiveSaliency/optLayout/";
